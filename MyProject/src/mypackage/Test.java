ae061a9b786da85da0113a3c5198cd2c8427cf9f
ghause.mr <ghause.mr@GHAUSE-MR01.sa.corp.samsungelectronics.net>
2018-04-11 21:46:48 +0530

    Commit and push

---
+++ MyProject/src/mypackage/Test.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java");
+		//System.setProperty("webdriver.chrome.driver", "D:\\automationstuff\\chromedriver_win32\\chromedriver.exe");
+		//WebDriver d=new ChromeDriver();
+		//d.get("http://chromedriver.storage.googleapis.com/index.html?path=2.36/");
+		
+	}
+
+}
