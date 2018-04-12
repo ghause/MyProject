8c4dcdbb78b5504bbeaf64e150d891c7eb7ac905
ghause.mr <ghause.mr@GHAUSE-MR01.sa.corp.samsungelectronics.net>
2018-04-13 00:52:59 +0530

    ...

--- MyProject/src/mypackage/Test1.java
+++ MyProject/src/mypackage/Test1.java
@@ -2,7 +2,11 @@ package mypackage;
 
 public class Test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		for(int i=0; i<=10; i++) {
+			Thread.sleep(1000);
 		System.out.println("My firts change in git hub");
+		}
+		
 	}
 }
